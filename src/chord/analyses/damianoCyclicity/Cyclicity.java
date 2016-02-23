50c5968406f6aa0ce3dbf62fc23661a1357352eb
Damiano Zanardini <damiano.zanardini@gmail.com>
2016-02-23 16:36:09 +0000

    <empty commit message>

--- src/chord/analyses/damianoCyclicity/Cyclicity.java
+++ src/chord/analyses/damianoCyclicity/Cyclicity.java
@@ -1,11 +1,29 @@
 package chord.analyses.damianoCyclicity;
 
+import java.util.Set;
+
+import joeq.Class.jq_Method;
+import joeq.Compiler.Quad.CodeCache;
+import joeq.Compiler.Quad.ControlFlowGraph;
+import joeq.Compiler.Quad.PrintCFG;
+import chord.analyses.alias.CSCG;
+import chord.analyses.alias.CSCGAnalysis;
+import chord.analyses.alias.Ctxt;
+import chord.analyses.alias.ICSCG;
 import chord.analyses.damianoAnalysis.Utilities;
+import chord.analyses.method.DomM;
+import chord.bddbddb.Rel;
+import chord.bddbddb.Rel.PairIterable;
+import chord.bddbddb.Rel.RelView;
+import chord.program.Program;
 import chord.project.Chord;
+import chord.project.ClassicProject;
 import chord.project.analyses.JavaAnalysis;
+import chord.project.analyses.ProgramRel;
+import chord.util.tuple.object.Pair;
 
 @Chord(name = "cyclicity",
-       consumes = { "P", "I", "M", "V", "F", "AbsF", "FSet", "VT", "Register", "UseDef" },
+       consumes = { "P", "I", "M", "V", "F", "AbsF", "FSet", "VT", "Register", "UseDef", "C", "CH", "CI", "reachableCM" },
        produces = { }
 )
 public class Cyclicity extends JavaAnalysis {
@@ -14,10 +32,9 @@ public class Cyclicity extends JavaAnalysis {
     	Utilities.setVerbose(true);
     	
     	CyclicityFixpoint fp = new CyclicityFixpoint();
-
     	fp.init();
     	fp.run();
-    	    	
+    	
     	fp.printOutput();
     }
 
