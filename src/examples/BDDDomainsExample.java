647e9eec9535f5e44783c6b2bb03f6910286d153
Miguel García Biedma <mgbiedma@gmail.com>
2017-06-14 10:56:40 +0000

    <empty commit message>

---
+++ src/examples/BDDDomainsExample.java
@@ -0,0 +1,72 @@
+package examples;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import net.sf.javabdd.BDD;
+import net.sf.javabdd.BDD.AllSatIterator;
+import net.sf.javabdd.BDDDomain;
+import net.sf.javabdd.BDDFactory;
+
+public class BDDDomainsExample {
+	// number of variables
+	static int N;
+	static int numberOfConditions;
+	static BDDFactory B;
+	static BDD[] X; /* BDD with conditions array */
+	// This two will eventually turn into a Map. This matches the var number with the array position 
+	static String[] vars = new String[]{"lparent", "lleft", "lright", "rparent", "rleft", "rright"};
+	static List<String> varList = Arrays.asList(vars);
+    static BDDFactory bdd;
+    static int n = 3; // 3x3 cube
+    static int k = 6; // number of moves
+
+	public static void main(String[] args) {
+		//init(6, 10);
+        bdd = BDDFactory.init("java",1000000, 100000);
+        bdd.setMaxIncrease(250000);
+
+		// still doing this manually
+        // 6 posibles valores para cada variable (0 a 5)
+        int numregs = 60;
+        long nbits= (long) (Math.ceil(Math.log(numregs)/Math.log(2)));
+        // TODO mejor desplazar bits
+        //long bitOffsetR = (long) Math.pow(2, nbits);
+        long bitOffsetR = 1 << nbits;
+
+        BDDDomain registers = bdd.extDomain(bitOffsetR*bitOffsetR);
+        BDD reg5l = registers.ithVar(6);
+        System.out.println("reg5l: " + reg5l);
+
+        System.out.println(bitOffsetR);
+        BDD reg5r = registers.ithVar(6*bitOffsetR);
+        System.out.println("reg5r: " + reg5r);
+        
+        BDD regX = registers.ithVar(6+6*bitOffsetR);
+        System.out.println("regX: " + regX);
+        System.out.println("??????");
+        System.out.println("??????");
+
+        // ---
+        BDDFactory bdd2 = BDDFactory.init("java",1000, 100);
+        bdd2.setMaxIncrease(250000);
+        bdd2.setVarNum(6);
+
+        BDD reg0Manual= bdd.one();
+        reg0Manual.andWith(bdd2.nithVar(0)).andWith(bdd2.nithVar(1)).andWith(bdd2.nithVar(2));
+        System.out.println("=======");
+        System.out.println(reg0Manual);
+
+        BDD reg2Manual = bdd.one();
+        reg2Manual.andWith(bdd2.nithVar(0)).andWith(bdd2.ithVar(1)).andWith(bdd2.nithVar(2));
+        System.out.println(reg2Manual);
+        BDD andR0R2 = reg0Manual.andWith(reg2Manual.id());
+        System.out.println("and: " + andR0R2);
+        BDD orR0R2 = reg0Manual.or(reg2Manual);
+        System.out.println("or: " + orR0R2);
+        
+        System.out.println(andR0R2.support());
+        BDD regSet = registers.set();
+        BDD iterBdd = bdd.one();
+
+	}
+}
