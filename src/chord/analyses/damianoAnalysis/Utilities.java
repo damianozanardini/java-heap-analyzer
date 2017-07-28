b58d435d28d32eccc294161d03d62aeb4908d23d
damiano <damiano@192.168.1.104>
2017-07-28 15:09:59 +0200

    Ordering on registers

--- src/chord/analyses/damianoAnalysis/Utilities.java
+++ src/chord/analyses/damianoAnalysis/Utilities.java
@@ -12,10 +12,27 @@ import chord.project.ClassicProject;
 
 public class Utilities {
 	
+	/**
+	 * Whether the analysis output is verbose or not.
+	 */
 	private static boolean verbose;
+
+	/**
+	 * Every time a message is output, this variable determines how much it is
+	 * indented from the left.  It is manipulated by begin(), which shifts 3 characters
+	 * to the right by calling shiftRight(), and end(), which shifts 3 characters
+	 * to the left by calling shiftLeft().
+	 */
 	private static String indent = "";
 	
+	/**
+	 * Adds 3 empty spaces to indent.
+	 */
 	private static void shiftRight() { indent = indent + "   "; }
+	
+	/**
+	 * Removes 3 empty spaces from indent.
+	 */
 	private static void shiftLeft() { indent = indent.substring(0, indent.length()-3); }
 	
 	public static void out(String str) { System.out.println(indent + str); }
@@ -60,10 +77,23 @@ public class Utilities {
 	public static void wp() { out("UNTIL HERE"); }
 	public static void wp(Object o) { out("UNTIL HERE - " + o); }
 	
+	/**
+	 * Tells whether the analysis output is verbose or not.
+	 *  
+	 * @return
+	 */
 	public static boolean isVerbose() { return verbose; }
 
+	/**
+	 * Sets the verbosity level (true or false).
+	 * 
+	 * @param verbose
+	 */
 	public static void setVerbose(boolean verbose) { Utilities.verbose = verbose; }
 
+	/**
+	 * Print the Control Flow Graph of each method in the program to be analyzed.
+	 */
 	public static void printCFGs() {
 		ArrayList<jq_Method> alreadyPrinted = new ArrayList<jq_Method>();
 		ControlFlowGraph cfg;
@@ -82,14 +112,43 @@ public class Utilities {
 		}	
 	}
 	
+	/**
+	 * Order on registers goes like this: regular registers ("R") precede temporal
+	 * registers ("T"), and smaller number precede bigger numbers. This is not exactly
+	 * lexicographic order on the String representation because we want R9 to precede
+	 * R10 (not the other way around, as in lexicographic).
+	 * 
+	 * @param r1
+	 * @param r2
+	 * @return
+	 */
 	public static boolean leqReg(Register r1,Register r2) {
-		return (r1.toString().compareTo(r2.toString()) <= 0);
+		String s1 = r1.toString();
+		String s2 = r2.toString();
+		int n1 = Integer.parseInt(s1.substring(1));
+		int n2 = Integer.parseInt(s2.substring(1));
+		return (s1.substring(0,1).compareTo(s2.substring(0,1)) < 0 ||
+				(s1.substring(0,1).compareTo(s2.substring(0,1)) == 0 &&
+				n1 <= n2));
 	}
 
+	/**
+	 * Minimum on registers.
+	 * 
+	 * @param r1
+	 * @param r2
+	 * @return
+	 */
 	public static Register minReg(Register r1,Register r2) {
 		return leqReg(r1,r2) ? r1 : r2;
 	}
 
+	/**
+	 * Maximum on registers.
+	 * @param r1
+	 * @param r2
+	 * @return
+	 */
 	public static Register maxReg(Register r1,Register r2) {
 		return leqReg(r1,r2) ? r2 : r1;
 	}
